7f09aa3e91a00216e9e6456864649478eeae873d
zililau010 <zililau010@gmail.com>
2024-10-17 16:20:13 +0800

    home page

---
+++ home.php
@@ -0,0 +1,76 @@
+<html>
+    <head>
+        <title>Huan Fitness</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="navbar">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Physical Training</a></li>
+                <li><a href="#">Membership</a></li>
+                <li><a href="#">Book Now</a></li>
+            </ul>
+        </div>
+
+            <div class="box">
+
+                <div class="content">
+                    <h1>Dashboard</h1>
+
+                    <div class="card">
+                        <div class="details">
+                            <div class="exercise-routine"></div>
+                            <div class="water-consumtion"></div>
+                            <div class="body-weight"></div>
+                            <button class="add-btn">+</button>
+                        </div>
+                    </div> 
+
+                    <div class="card">
+                        <div class="date">
+                            <div class="day">22</div>
+                            <div class="month">Nov</div>
+                        </div>
+                        <div class="details">
+                            <div class="exercise-routine">Exercise Routine: Jogging</div>
+                            <div class="water-consumtion">Water Consumtion: 2 litters</div>
+                            <div class="body-weight">Body Weight(kg): 20</div>
+                            <button class="update-btn">Update</button>
+                            <button class="delete-btn">Delete</button>
+                        </div>
+                    </div>
+
+                    <div class="card">
+                        <div class="date">
+                            <div class="day">22</div>
+                            <div class="month">Nov</div>
+                        </div>
+                        <div class="details">
+                            <div class="exercise-routine">Exercise Routine: Jogging</div>
+                            <div class="water-consumtion">Water Consumtion: 2 litters</div>
+                            <div class="body-weight">Body Weight(kg): 20</div>
+                            <button class="update-btn">Update</button>
+                            <button class="delete-btn">Delete</button>
+                        </div>
+                    </div>
+
+                    <div class="card">
+                        <div class="date">
+                            <div class="day">22</div>
+                            <div class="month">Nov</div>
+                        </div>
+                        <div class="details">
+                            <div class="exercise-routine">Exercise Routine: Jogging</div>
+                            <div class="water-consumtion">Water Consumtion: 2 litters</div>
+                            <div class="body-weight">Body Weight(kg): 20</div>
+                            <button class="update-btn">Update</button>
+                            <button class="delete-btn">Delete</button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+    </body>
+</html>
